e0e7e8248021f888ff92c262cc8b261cb8d68cb0
lemontime <139415938+lemontime0106@users.noreply.github.com>
2025-02-23 12:03:08 +0900

    env 설정

--- FE/src/firebaseConfig.js
+++ FE/src/firebaseConfig.js
@@ -1,20 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+const {
+  VITE_FE_apiKey,
+  VITE_FE_authDomain,
+  VITE_FE_databaseURL,
+  VITE_FE_projectId,
+  VITE_FE_storageBucket,
+  VITE_FE_messagingSenderId,
+  VITE_FE_appId,
+  VITE_FE_measurementId,
+} = import.meta.env;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1ufIQF3Hk0_JztNY28XuIGhbW-v6IegA",
-  authDomain: "amuz-project-c2c85.firebaseapp.com",
-  databaseURL: "https://amuz-project-c2c85-default-rtdb.firebaseio.com",
-  projectId: "amuz-project-c2c85",
-  storageBucket: "amuz-project-c2c85.firebasestorage.app",
-  messagingSenderId: "788919011251",
-  appId: "1:788919011251:web:52c3e6e92934f98aa62dc6",
-  measurementId: "G-MXES1M6K9K",
+  apiKey: VITE_FE_apiKey,
+  authDomain: VITE_FE_authDomain,
+  databaseURL: VITE_FE_databaseURL,
+  projectId: VITE_FE_projectId,
+  storageBucket: VITE_FE_storageBucket,
+  messagingSenderId: VITE_FE_messagingSenderId,
+  appId: VITE_FE_appId,
+  measurementId: VITE_FE_measurementId,
 };
 
 // Initialize Firebase
